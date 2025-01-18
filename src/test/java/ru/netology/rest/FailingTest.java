a30e1027c4eb50839a40bc0a41e011280f67e772
Aleks Danilushkin <thenumber@ya.ru>
2025-01-18 14:23:30 +0300

    Disable failing test

--- src/test/java/ru/netology/rest/FailingTest.java
+++ src/test/java/ru/netology/rest/FailingTest.java
@@ -1,12 +1,14 @@
-package ru.netology.rest;
+package ru.netology;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class FailingTest {
+class FailingTest {
 
     @Test
+    @Disabled("Отключено для проверки CI")
     void testWithError() {
         assertEquals(1, 2, "Этот тест должен провалиться");
     }
