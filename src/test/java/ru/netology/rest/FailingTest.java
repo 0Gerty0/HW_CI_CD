7610604a4da1545f38b9f4cb1f6acc31cb7491aa
Aleks Danilushkin <thenumber@ya.ru>
2025-01-18 14:01:34 +0300

    Fix syntax error in FailingTest.java

--- src/test/java/ru/netology/rest/FailingTest.java
+++ src/test/java/ru/netology/rest/FailingTest.java
@@ -1,16 +1,13 @@
 package ru.netology.rest;
 
-package ru.netology;
-
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class FailingTest {
+public class FailingTest {
 
     @Test
     void testWithError() {
-        // Намеренная ошибка
         assertEquals(1, 2, "Этот тест должен провалиться");
     }
 }
